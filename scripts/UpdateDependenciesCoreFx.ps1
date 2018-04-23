b98632155a78b2c3d48c7d9bd515f29cf15daf0c
Ryan Brandenburg <rybrande@microsoft.com>
2018-04-23 12:29:01 -0700

    Target master equivelent branch for master dependencies

--- scripts/UpdateDependenciesCoreFx.ps1
+++ scripts/UpdateDependenciesCoreFx.ps1
@@ -103,7 +103,7 @@ Write-Host "Loading deps from $depsPath"
 [xml] $dependencies = LoadXml $depsPath
 
 $remote = "origin"
-$baseBranch = "release/2.1"
+$baseBranch = "dev"
 
 $currentBranch = Invoke-Block { & git rev-parse --abbrev-ref HEAD }
 $destinationBranch = "rybrande/UpgradeDepsTest"
